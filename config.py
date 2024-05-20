19dc09794cb9216b76cbdd149f527f1707c27815
Baji-kun <152496758+Baji-kun@users.noreply.github.com>
2024-05-20 20:10:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,23 +13,23 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 200))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-OWNER_USERNAME = getenv("OWNER_USERNAME","TheKidPersonOp")
-CHAT_GROUP = getenv("CHAT_GROUP","https://t.me/FRIENDS2FAMILY00")
-OWNER_ID = int(getenv("OWNER_ID", 6507819384))
+OWNER_USERNAME = getenv("OWNER_USERNAME","Itz_Zenoi")
+CHAT_GROUP = getenv("CHAT_GROUP","https://t.me/Series_and_Movies_Request_Group")
+OWNER_ID = int(getenv("OWNER_ID",5957500906))
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
-UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/cutexboy/MUSICALLBOTS",)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO","https://github.com/Baji-kun/M",)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/NeoUpdatess")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FRIENDS2FAMILY00")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL","https://t.me/+mKXIX38_UpMxOTg1")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Series_and_Movies_Request_Group")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
 AUTO_GCAST = getenv("AUTO_GCAST","True")
 AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
-PROMO = getenv("PROMO","https://graph.org/file/876eee7413536f03c5245.jpg")
+PROMO = getenv("PROMO","https://graph.org/file/9476659f5ddae89b5ae9e.jpg")
 TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
@@ -49,21 +49,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://te.legra.ph/file/25efe6aa029c6baea73ea.jpg"
+    "START_IMG_URL", "https://graph.org/file/9476659f5ddae89b5ae9e.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
+    "PING_IMG_URL", "https://graph.org/file/9476659f5ddae89b5ae9e.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+PLAYLIST_IMG_URL = "https://graph.org/file/9476659f5ddae89b5ae9e.jpg"
+STATS_IMG_URL = "https://graph.org/file/568cb0b03f8c80fd06e40.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/9476659f5ddae89b5ae9e.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/cf120a5879ee7af6fa5c8.jpg"
+STREAM_IMG_URL = "https://graph.org/file/086ebd3e7ae1d338ee123.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/2b01227df8735ac46fc43.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/5e0eca6f403baae74b063.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/3fe52b8f4fe34349f6c58.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/fef135796f1c62a0f93a8.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/689984de099a0edca9a7f.jpg"
 
 
 def time_to_seconds(time):
